8b176c1df1099b8eb4e4eb52a8c5a54f9cea4c8a
theopakieser <20101918@mail.wit.ie>
2024-12-23 16:47:41 +0000

    cleaned up user/index.js

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -33,7 +33,7 @@ router.post('/register', asyncHandler(async (req, res) => {
 }));
 
 //authenticate user
-router.post('/login', asyncHandler(async (req, res) => {
+router.post('/', asyncHandler(async (req, res) => {
     const { username, password } = req.body;
     if (!username || !password) {
         return res.status(400).json({ success: false, msg: 'Username and password are required.' });
@@ -63,34 +63,6 @@ router.put('/:id', async (req, res) => {
         res.status(404).json({ code: 404, msg: 'Unable to Update User' });
     }
 });
-
-// async function registerUser(req, res) {
-//     const { username, password} = req.body;
-//     const existingUser = await User.findOne({username});
-//     if (existingUser){
-//         throw new Error('User already exists');
-//     }else{
-//         const hashedPassword = await bcrypt.hash(password, 5);
-//         const newUser = new User({username, password: hashedPassword})
-//         await newUser.save();
-//     }
-//     res.status(201).json({ success: true, msg: 'User successfully created.' });
-// }
-
-// async function authenticateUser(req, res) {
-//     const user = await User.findByUserName(req.body.username);
-//     if (!user) {
-//         return res.status(401).json({ success: false, msg: 'Authentication failed. User not found.' });
-//     }
-
-//     const isMatch = await user.comparePassword(req.body.password);
-//     if (isMatch) {
-//         const token = jwt.sign({ username: user.username }, process.env.SECRET);
-//         res.status(200).json({ success: true, token: 'BEARER ' + token });
-//     } else {
-//         res.status(401).json({ success: false, msg: 'Wrong password.' });
-//     }
-// }
 
 
 export default router;
