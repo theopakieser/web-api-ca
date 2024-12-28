5dcb4444d41b9316d7c19ea70376b787f0e66798
theopakieser <20101918@mail.wit.ie>
2024-12-28 19:03:40 +0000

    get movie details updated

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,12 +1,31 @@
-import {
-    getUpcomingMovies, getGenres, getNowPlayingMovies, getPopularMovies
-  } from '../tmdb';
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
 import express from 'express';
-
+import { getUpcomingMovies, getGenres, getPopularMovies, getNowPlayingMovies, getMovieDetails, getMovies } from '../tmdb-api';
 
 const router = express.Router();
+
+// Get movie details
+router.get('/:id', asyncHandler(async (req, res) => {
+    const id = parseInt(req.params.id);
+    const movie = await movieModel.findByMovieDBId(id);
+    if (movie) {
+        res.status(200).json(movie);
+    } else {
+        res.status(404).json({message: 'The movie you requested could not be found.', status_code: 404});
+    }
+}));
+
+// Get movie details
+router.get('/tmdb/discover/:id', asyncHandler(async (req, res) => {
+    const id = parseInt(req.params.id);
+    const movie = await movieModel.findByMovieDBId(id);
+    if (movie) {
+        res.status(200).json(movie);
+    } else {
+        res.status(404).json({message: 'The movie you requested could not be found.', status_code: 404});
+    }
+}));
 
 router.get('/', asyncHandler(async (req, res) => {
     let { page = 1, limit = 10 } = req.query; // destructure page and limit and set default values
@@ -29,30 +48,19 @@ router.get('/', asyncHandler(async (req, res) => {
     res.status(200).json(returnObject);
 }));
 
-// Get movie details
-// router.get('/:id', asyncHandler(async (req, res) => {
-//     const id = parseInt(req.params.id);
-//     const movie = await movieModel.findByMovieDBId(id);
-//     if (movie) {
-//         res.status(200).json(movie);
-//     } else {
-//         res.status(404).json({message: 'The movie you requested could not be found.', status_code: 404});
-//     }
-// }));
-
-router.get('/tmdb/upcoming', asyncHandler(async (req, res) => {
-    const upcomingMovies = await getUpcomingMovies();
-    res.status(200).json(upcomingMovies);
+router.get('', asyncHandler(async (req, res) => {
+    const movies = await getMovies();
+    res.status(200).json(movies);
 }));
 
 router.get('/tmdb/genres', asyncHandler(async (req, res) => {
     const genres = await getGenres();
     res.status(200).json(genres);
-}))
+}));
 
-router.get('/tmdb/now_playing', asyncHandler(async (req, res) => {
-    const nowPlayingMovies = await getNowPlayingMovies();
-    res.status(200).json(nowPlayingMovies);
+router.get('/tmdb/upcoming', asyncHandler(async (req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
 }));
 
 router.get('/tmdb/popular', asyncHandler(async (req, res) => {
@@ -60,9 +68,24 @@ router.get('/tmdb/popular', asyncHandler(async (req, res) => {
     res.status(200).json(popularMovies);
 }));
 
-router.get('/', asyncHandler(async (req, res) => {
-    const movies = await movieModel.find();
+router.get('/tmdb/now_playing', asyncHandler(async (req, res) => {
+    const nowPlayingMovies = await getNowPlayingMovies();
+    res.status(200).json(nowPlayingMovies);
+}));
+
+router.get('/tmdb/discover', asyncHandler(async (req, res) => {
+    const movies = await getMovies();
     res.status(200).json(movies);
 }));
+
+router.get('/tmbd/discover/:id', async (req, res) => {
+    const id = req.params.id; // Extract the dynamic parameter
+    try {
+        const movieDetails = await getMovieDetails(id);
+        res.json(movieDetails);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
 
 export default router;
--- movies-api/api/tmdb.js
+++ movies-api/api/tmdb.js
@@ -68,7 +68,7 @@ export const getNowPlayingMovies = async () => {
 export const getMovieDetails = async () => {
     try {
         const response = await fetch(
-            `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.TMDB_KEY}&language=en-US`
+            `https://api.themoviedb.org/3/movie/discover/${movieId}?api_key=${process.env.TMDB_KEY}&language=en-US`
         );
 
         if (!response.ok) {
